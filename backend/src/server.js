621f82619aa2566a03ed8e719c7820b9326341a3
Márcio Xavier <43189549+xaviermarcio@users.noreply.github.com>
2023-12-28 16:56:08 -0300

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes.js');
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.get('/',(req,res)=>{
